1ad3ec3f733e4c0a72ae1937bcf842fc030b5ecc
ivanyukgg <ivanyuk.ua.gg.@gmail.com>
2024-10-11 07:45:41 +0300

    с++
    вьвіь

---
+++ Untitled-1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    float x;
+    x = sqrt(2);
+    for (int i = 2; i <= n; i++)
+    {
+        x = sqrt(2+x);
+    }
+    cout<<x<<endl;
+}
